349441eba7c75945830358af1a752d24629e9e6b
Blue-bg <antoine.miguez@gmail.com>
2023-06-03 17:28:05 +0200

    mariadb and wordpress done

---
+++ requirements/wordpress/tools/launch_wp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if ! wp core is-installed --allow-root --path=/var/www/html/wordpress
+then
+
+wp core install	--allow-root \
+				--path=/var/www/html/wordpress \
+				--url=$DOMAIN_URL \
+				--title=$DOMAIN_TITLE \
+				--admin_user=$WP_ADMIN_USER \
+				--admin_password=$WP_ADMIN_PASS \
+				--admin_email=$WP_ADMIN_MAIL
+
+wp user create	--allow-root \
+				--path=/var/www/html/wordpress \
+				$USER_LOGIN \
+				$USER_MAIL \
+				--user_pass=$USER_PASS
+
+fi
+
+php-fpm7.3 -F
